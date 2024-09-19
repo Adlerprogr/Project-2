ba4f49515f5a9b9cf029464cdb9ca795a6f9df73
Adlerprogr <zhargalovald@yandex.ru>
2024-09-19 20:54:48 +0900

    Перенес окружение из Project-1, проверил, работает. Подключил composer и Laravel, проверил, раюотает, выдаёт приветственную страницу Laravel в браузере. Добавил миграции, создал основные таблицы для своего проекта. Подключил базу данных и запустил миграции, все работает правильно.

---
+++ database/migrations/2024_09_18_154809_create_products.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category'); // Предполагая "category" таблица существует
+            $table->string('name');
+            $table->text('description');
+            $table->integer('calories');
+            $table->integer('squirrels');
+            $table->integer('fats');
+            $table->integer('carbohydrates');
+            $table->integer('weight');
+            $table->decimal('price', 10, 2);
+            $table->unsignedBigInteger('image_id');
+            $table->foreign('image_id')->references('id')->on('images'); // Предполагая "images" таблица существует
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_09_18_154835_create_user_product.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_product', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users'); // Assuming "users" table exists
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products'); // Assuming "products" table exists
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_product');
+    }
+};
